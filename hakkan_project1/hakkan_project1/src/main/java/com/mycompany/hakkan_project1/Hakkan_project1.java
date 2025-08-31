bfd4d7b8750e7e4e31b86f6a6fdb91c0678d85a5
hakkanazrul06 <azrulsusenohakkan@gmail.com>
2025-08-31 20:19:58 +0700

    Add files via upload

---
+++ hakkan_project1/hakkan_project1/src/main/java/com/mycompany/hakkan_project1/Hakkan_project1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hakkan_project1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Hakkan_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
