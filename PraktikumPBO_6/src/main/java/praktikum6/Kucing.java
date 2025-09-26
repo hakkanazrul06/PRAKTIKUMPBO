53ba1a4ffe9d3fae88e50fe6e4eb5f7ef705abb7
hakkanazrul06 <azrulsusenohakkan@gmail.com>
2025-09-26 11:18:05 +0700

    Add files via upload

---
+++ PraktikumPBO_6/src/main/java/praktikum6/Kucing.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum6;
+
+/**
+ *
+ * @author LENOVO
+ */
+class Kucing extends Hewan {
+    @Override
+    public void bersuara() {
+        System.out.println("Meow");
+    }
+}
+  
+
